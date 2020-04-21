eb0facd9a708d23c559db36f30f99d402f83bcd2
Manan Sahni <manan.sahni@nagarro.com>
2020-04-21 23:11:42 +0530

    add input function to the assignment2component

--- src/app/05_assignment/02_assignment2/containers/assignment2.component.ts
+++ src/app/05_assignment/02_assignment2/containers/assignment2.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component,Input } from '@angular/core';
+import { Todo } from '../../assignment.component';
+
 
 @Component({
   selector: 'app-assignment2',
   templateUrl: './assignment2.component.html',
   styleUrls: ['./assignment2.component.css']
 })
-export class Assignment2Component {
 
+
+export class Assignment2Component {
+  @Input()
+  todos: Todo[];
+  
+  selectedTodo: Todo;
+  
+  onSelectedTodo(todo: Todo){
+    this.selectedTodo = todo;
+  }
+ 
 }
